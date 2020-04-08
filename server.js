c077fee788404bf2013cfaaef169a144267fe383
Manikandan Sundaramoorthy <smanihwr@gmail.com>
2020-04-08 17:22:07 -0500

    40. Backend Routes

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express'); // import is not supported - need to use babel to import
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Welcome to contact keeper api' });
+});
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+app.listen(5000, () => console.log(`Server started on port ${PORT}`));
